f66c7be5fb4572d36a4f0671a3e848804b996c2f
Adil Nurkali <a.nurkaliyev@platform.live>
2023-05-27 12:37:17 +0200

    finish services section

--- src/app/components/Services/Services.tsx
+++ src/app/components/Services/Services.tsx
@@ -10,7 +10,14 @@ import serviceEducation from '../../../../public/assets/images/service-education
 import serviceRent from '../../../../public/assets/images/service-rent.png';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
 import 'swiper/css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Navigation } from 'swiper';
+import 'swiper/css/navigation';
 
 const SERVICES = [
   {
@@ -53,7 +60,7 @@ const SERVICES = [
 const Services = () => {
   return (
     <section className="pt-10 lg:pt-16 lg:pb-16">
-      <div className="mb-12 lg:mb-16 text-neutral-900">
+      <div className="mb-12 text-neutral-900">
         <h2 className="font-medium text-neutral-800 text-4xl md:text-5xl xl:text-5xl !leading-[114%]">
           Популярные услуги
         </h2>
@@ -61,38 +68,51 @@ const Services = () => {
           Услуги пользующиеся наибольшим спросом
         </p>
       </div>
-      <Swiper
-        spaceBetween={30}
-        slidesPerView={5}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        {SERVICES.map((service, i) => (
-          <SwiperSlide key={i}>
-            <div className="">
-              <div className="w-full">
-                <Image
-                  src={service.img}
-                  alt="mainSection"
-                  // width={224}
-                  // height={168}
-                  //   style={{ objectFit: 'cover', height: '168px', width: '224px' }}
-                  // fill
-                  className="rounded-2xl overflow-hidden"
-                />
+      <div className="relative">
+        <Swiper
+          spaceBetween={30}
+          slidesPerView={5}
+          modules={[Navigation]}
+          navigation={{
+            prevEl: '.prev',
+            nextEl: '.next',
+          }}
+        >
+          {SERVICES.map((service, i) => (
+            <SwiperSlide key={i}>
+              <div className="">
+                <div className="w-full">
+                  <Image
+                    src={service.img}
+                    alt="mainSection"
+                    style={{
+                      objectFit: 'cover',
+                      height: '168px',
+                    }}
+                    className="rounded-2xl overflow-hidden"
+                  />
+                </div>
+                <div className="mt-4 px-3">
+                  <p className="text-base sm:text-lg text-neutral-900 font-medium whitespace-normal">
+                    {service.title}
+                  </p>
+                  <p className="block mt-2 text-sm text-neutral-6000">
+                    {service.text}
+                  </p>
+                </div>
               </div>
-              <div className="mt-4 px-3">
-                <p className="text-base sm:text-lg text-neutral-900 dark:text-neutral-100 font-medium whitespace-normal">
-                  {service.title}
-                </p>
-                <p className="block mt-2 text-sm text-neutral-6000 dark:text-neutral-400">
-                  {service.text}
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="absolute -top-24 right-0 mt-4 px-3 flex items-center text-neutral-900">
+          <button className="prev w-10 h-10 mr-[6px] bg-white border border-neutral-200 rounded-full flex items-center justify-center hover:border-neutral-300 focus:outline-none">
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          <button className="next w-10 h-10 bg-white border border-neutral-200 rounded-full flex items-center justify-center hover:border-neutral-300 focus:outline-none">
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
