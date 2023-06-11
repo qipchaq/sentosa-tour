b9f9c4fb3ea5609a6edd84ea663f7af3511a2ce4
Adil Nurkali <a.nurkaliyev@platform.live>
2023-06-11 13:23:55 +0200

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,33 +13,6 @@ const roboto = Roboto({
   subsets: ['cyrillic', 'latin'],
 });
 
-export const metadata = {
-  title:
-    'Sentosa tour - авторизированный партнер иммиграционного офиса Сингапура',
-  description:
-    'Sentosa tour предлагает оформить визу в Сингапур онлайн. Для граждан России, СНГ, Грузии, Туркменистана, Молдовы, Не граждан Латвии и Эстонии',
-  icons: {
-    icon: { url: '/assets/images/logo.png', type: 'image/png' },
-  },
-  openGraph: {
-    title:
-      'Sentosa tour - авторизированный партнер иммиграционного офиса Сингапура',
-    description:
-      'Sentosa tour предлагает оформить визу в Сингапур онлайн. Для граждан России, СНГ, Грузии, Туркменистана, Молдовы, Не граждан Латвии и Эстонии',
-    url: 'https://www.sentosatours.kz/',
-    siteName: 'Sentosa tours',
-    images: [
-      {
-        url: 'https://www.sentosatours.kz/assets/images/logo.png',
-        width: 200,
-        height: 200,
-      },
-    ],
-    locale: 'ru-RU',
-    type: 'website',
-  },
-};
-
 export default function RootLayout({
   children,
 }: {
