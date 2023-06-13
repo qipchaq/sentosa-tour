15876b1593e1688a480334f87df9e5fadc8c3971
Adil Nurkali <a.nurkaliyev@platform.live>
2023-06-13 22:45:04 +0200

    refactoring metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,42 @@
 import GoogleAnalytics from '../../service/utils/GoogleAnalytics';
 import SchemaOrg from '../../service/utils/SchemaOrg';
-// import { schema } from '../../service/utils/schema';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import { metaData } from './components/shared/metaData';
+// import { metaData } from './components/shared/metaData';
 import './globals.css';
 import { Roboto } from 'next/font/google';
-// import Head from 'next/head';
 
 const roboto = Roboto({
   weight: ['400', '500', '700'],
   subsets: ['cyrillic', 'latin'],
 });
+
+export const metaData = {
+  title:
+    'Sentosa tour - авторизированный партнер иммиграционного офиса Сингапура',
+  description:
+    'Sentosa tour предлагает оформить визу в Сингапур онлайн. Для граждан России, СНГ, Грузии, Туркменистана, Молдовы, Не граждан Латвии и Эстонии',
+  icons: {
+    icon: { url: '/assets/images/logo.png', type: 'image/png' },
+  },
+  openGraph: {
+    title:
+      'Sentosa tour - авторизированный партнер иммиграционного офиса Сингапура',
+    description:
+      'Sentosa tour предлагает оформить визу в Сингапур онлайн. Для граждан России, СНГ, Грузии, Туркменистана, Молдовы, Не граждан Латвии и Эстонии',
+    url: 'https://www.sentosatours.kz/',
+    siteName: 'Sentosa tours',
+    images: [
+      {
+        url: '/assets/images/logo.png',
+        width: 200,
+        height: 200,
+      },
+    ],
+    locale: 'ru-RU',
+    type: 'website',
+  },
+};
 
 export const metadata = metaData;
 
@@ -22,13 +47,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <Head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
-          key="product-jsonld"
-        />
-      </Head> */}
       <body
         className={`bg-white text-base text-neutral-900 min-h-screen ${roboto.className}`}
       >
