0078eeee677e02644398fe488d87f8c77f52b419
Adil Nurkali <a.nurkaliyev@platform.live>
2023-06-17 13:52:29 +0200

    add product type schema

--- service/utils/SchemaOrg.tsx
+++ service/utils/SchemaOrg.tsx
@@ -22,6 +22,52 @@ const schema = {
   ],
 };
 
+const schemaProduct = {
+  '@context': 'http://schema.org',
+  '@type': 'Product',
+  url: 'https://www.sentosatour.kz/',
+  name: 'Sentosa tour - Визы в Сингапур',
+  image: ['https://www.sentosatours.kz/assets/images/logo.png'],
+  description:
+    'Sentosa tour предлагает оформить визу в Сингапур онлайн. Для граждан России, СНГ, Грузии, Туркменистана, Молдовы, Не граждан Латвии и Эстонии',
+  sku: '65590697',
+  mpn: 'GL6B-5MPFL',
+  brand: { '@type': 'Brand', name: 'Визы в Сингапур' },
+  aggregateRating: {
+    '@type': 'AggregateRating',
+    ratingValue: 5,
+    reviewCount: 8,
+  },
+  offers: {
+    '@type': 'Offer',
+    url: 'https://www.sentosatour.kz/visastart',
+    price: '60',
+    priceCurrency: 'USD',
+    availability: 'https://schema.org/Backorder',
+  },
+  breadcrumb: {
+    '@type': 'BreadcrumbList',
+    itemListElement: [
+      {
+        '@type': 'ListItem',
+        position: 1,
+        item: {
+          name: 'Визы в Сингапур',
+          '@id': 'https://www.sentosatour.kz/visastart',
+        },
+      },
+      {
+        '@type': 'ListItem',
+        position: 2,
+        item: {
+          name: 'Статус заявки',
+          '@id': 'https://www.sentosatour.kz/visacheck',
+        },
+      },
+    ],
+  },
+};
+
 export default function SchemaOrg() {
   return (
     <>
@@ -29,6 +75,11 @@ export default function SchemaOrg() {
         id="schema"
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      />
+      <Script
+        id="schema"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaProduct) }}
       />
     </>
   );
